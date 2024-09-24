8a1c68e21630f628ecd32cf9c03f7d0870791ffa
ss707494 <ss707494@163.com>
2024-09-24 19:46:04 +0800

    feat(ai): add account page and related components

---
+++ components/TopSectionBackground/TopSectionBackground.tsx
@@ -0,0 +1,24 @@
+import { ImagesField } from '@components/Home/Container'
+
+export const TopSectionBackground = () => {
+  return (
+    <ImagesField>
+      <img
+        className='w-screen absolute top-0 left-0'
+        src='/images/product/product-top.png'
+        alt='product-top'
+      />
+      <img
+        className='rotate-[276deg] absolute top-[140px] -right-[28px] w-[156px] h-[156px]
+          blur-[4.6px]'
+        src='/images/product/product-dot.png'
+        alt='product-dot'
+      />
+      <img
+        className='absolute rotate-[45deg] top-[300px] -left-[73px] w-[245px] h-[245px]'
+        src='/images/product/product-dot.png'
+        alt='product-dot'
+      />
+    </ImagesField>
+  )
+}
