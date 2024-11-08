b66940361acfc43dca4431aa2cd620afe43c80f0
Rick Chi <wenzhongchi@gmail.com>
2024-11-08 16:09:45 -0500

    fix: update

--- next.config.js
+++ next.config.js
@@ -25,7 +25,8 @@ const nextConfig = {
       ? [
           {
             source: '/api/graphql/:path*',
-            destination: `${process.env.NEXT_PUBLIC_GRAPHQL_API_URL}/:path*`
+            destination: `${process.env.NEXT_PUBLIC_GRAPHQL_API_URL}/:path*`,
+            basePath: false
           },
           {
             source: '/api/:path*',
