c60473df63c0039677523eaba9393b37e87a8929
Rick Chi <wenzhongchi@gmail.com>
2024-11-25 19:01:27 -0500

    fix: update status

--- components/Layout/Header/Header.tsx
+++ components/Layout/Header/Header.tsx
@@ -47,7 +47,11 @@ const Header: FC<HeaderProps> = ({ className }) => {
 
   const isAuthorized = useMemo(() => {
     if (address) {
-      return (data ?? []).findIndex((item) => item.user === address) > -1
+      return (
+        (data ?? []).findIndex(
+          (item) => item.user.toLowerCase() === address.toLowerCase()
+        ) > -1
+      )
     }
     return false
   }, [address, data])
