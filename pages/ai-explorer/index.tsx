2155768a7f1c61c5dccd14585f0d054b01147647
Jiang Li <lijiang0327@gmail.com>
2024-12-29 00:31:00 +0800

    feat: new ai

--- pages/ai-explorer/index.tsx
+++ pages/ai-explorer/index.tsx
@@ -1,9 +1,12 @@
 import { useCallback, useEffect, useState } from 'react'
+import { useTranslations } from 'next-intl'
+import clsx from 'clsx'
 import { useAccount } from 'wagmi'
 
 import { ApolloProvider } from '@apollo/client'
 import ConversationComponent from '@components/AIExplorer/ConversationComponent'
 import Sidebar from '@components/AIExplorer/Sidebar'
+import { Button } from '@components/Button'
 import { Container } from '@components/Home/Container'
 import Layout from '@components/Layout/Layout'
 import client from '@graphql/client/client'
@@ -15,10 +18,14 @@ import { useStore } from '@store/store'
 import { Conversation } from '@type/internal/conversation'
 import { getRandomId } from '@utils/random'
 
+const gradientBorderClass =
+  'border-transparent [background-clip:padding-box,border-box] [background-origin:padding-box,border-box] bg-[linear-gradient(to_right,#151515,#151515),linear-gradient(to_bottom,rgba(231,137,255,1)_0%,rgba(146,153,255,1)_100%)]'
+
 const AIExplorer = () => {
   const { address, isConnected } = useAccount()
   const [activeConversationId, setActiveConversationId] = useState<string>('')
   const [isSidebarOpen, onSidebarChange] = useState(false)
+  const t = useTranslations('Ai')
 
   const {
     data: history,
@@ -122,7 +129,36 @@ const AIExplorer = () => {
         className='w-[1440px] max-auto h-[100vh] px-2 md:px-10 pt-[120px] pb-[140px] sm:pt-[160px]
           sm:pb-[160px] overflow-hidden'
       >
-        <div className='w-full h-full relative overflow-hidden rounded-[32px]'>
+        <div
+          className={clsx(
+            `p-4 mb-4 grid grid-cols-3 gap-4 md:px-8 md:py-4 border-2 rounded-[20px]
+            md:backdrop-filter md:backdrop-blur-[10px]`,
+            gradientBorderClass
+          )}
+        >
+          <div
+            className='bg-black rounded-[16px] h-[55px] w-full px-4 text-[18px] text-co-gray-7 flex
+              justify-between items-center'
+          >
+            <span className=''>{t('totalNetworkDeposits')}</span>
+            <span>--</span>
+          </div>
+          <div
+            className='bg-black rounded-[16px] h-[55px] w-full px-4 text-[18px] text-co-gray-7 flex
+              justify-between items-center'
+          >
+            <span className=''>{t('totalNetworkUsageCount')}</span>
+            <span>--</span>
+          </div>
+          <div
+            className='bg-black rounded-[16px] h-[55px] w-full px-4 text-[18px] text-co-gray-7 flex
+              justify-between items-center'
+          >
+            <span className=''>{t('totalTokensConsumed')}</span>
+            <span>--</span>
+          </div>
+        </div>
+        <div className='w-full h-[55vh] relative overflow-hidden rounded-[32px]'>
           <Sidebar
             activeConversationId={activeConversationId}
             createNewConversation={createNewConversation}
@@ -132,6 +168,62 @@ const AIExplorer = () => {
             onSidebarChange={onSidebarChange}
           />
           <ConversationComponent conversationId={activeConversationId} />
+        </div>
+        <div
+          className={clsx(
+            'p-4 mb-4 grid grid-cols-5 gap-4 md:px-8 md:py-4 border-2 my-4',
+            'rounded-[20px] md:backdrop-filter md:backdrop-blur-[10px] border-[#666]'
+          )}
+        >
+          <div
+            className='bg-black flex-col gap-2 py-2 rounded-[16px] w-full px-4 text-[18px]
+              text-co-gray-7 flex justify-start items-center'
+          >
+            <span className='text-[14px] font-[600]'>
+              {t('totalNetworkDeposits')}
+            </span>
+            <span className='text-white text-[18px] font-[600]'>123456</span>
+            <Button size='sm' className='w-full h-[32px] rounded-full'>
+              {t('deposit')}
+            </Button>
+          </div>
+          <div
+            className='bg-black flex-col gap-2 py-2 rounded-[16px] w-full px-4 text-[18px]
+              text-co-gray-7 flex justify-start items-center'
+          >
+            <span className='text-[14px] font-[600]'>
+              {t('tokensConsumed')}
+            </span>
+            <span className='text-white text-[18px] font-[600]'>123456</span>
+          </div>
+          <div
+            className='bg-black flex-col gap-2 py-2 rounded-[16px] w-full px-4 text-[18px]
+              text-co-gray-7 flex justify-start items-center'
+          >
+            <span className='text-[14px] font-[600]'>{t('usageCount')}</span>
+            <span className='text-white text-[18px] font-[600]'>123456</span>
+          </div>
+          <div
+            className='bg-black flex-col gap-2 py-2 rounded-[16px] w-full px-4 text-[18px]
+              text-co-gray-7 flex justify-start items-center'
+          >
+            <span className='text-[14px] font-[600]'>
+              {t('remainingTokens')}
+            </span>
+            <span className='text-white text-[18px] font-[600]'>123456</span>
+            <Button size='sm' className='w-full h-[32px] rounded-full'>
+              {t('withdraw')}
+            </Button>
+          </div>
+          <div
+            className='bg-black flex-col gap-2 py-2 rounded-[16px] w-full px-4 text-[18px]
+              text-co-gray-7 flex justify-start items-center'
+          >
+            <span className='text-[14px] font-[600]'>
+              {t('remainingUsageCount')}
+            </span>
+            <span className='text-white text-[18px] font-[600]'>123456</span>
+          </div>
         </div>
       </Container>
     </Layout>
